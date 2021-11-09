74834b38edab19e7eff4e8a0a8d9fb9eb4559057
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-09 14:58:11 -0800

    Inserted badge into main content page

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 export default class MainContent extends Component {
+  state = { pageTitle: "Customers", customersCount: 5 };
+
   render() {
-    return <div> Hello from Main Content!</div>;
+    return (
+      <div>
+        <h4 className="border-bottom m-1 p-1">
+          {this.state.pageTitle}
+          <span className="badge bg-secondary m-2">
+            {this.state.customersCount}
+          </span>
+        </h4>
+      </div>
+    );
   }
 }
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,7 +3,7 @@ class NavBar extends Component {
   render() {
     return (
       <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/#">
               My App
