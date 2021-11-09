e51ca554d2b5f832512a9374a5025792a050f18a
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-08 16:41:40 -0800

    add navbar

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import "popper.js/dist/umd/popper";
 import "bootstrap/dist/js/bootstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./App";
+import "./index.css";
 
 ReactDOM.render(<NavBar />, document.getElementById("root"));
