8140585dd2ebd74d227168ee20aca52a91af9d3f
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-09 14:19:06 -0800

    adding navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,7 @@
 import React, { Component } from "react";
-class NavBar extends Component {
+import NavBar from "./NavBar";
+export default class App extends Component {
   render() {
-    return (
-      <React.Fragment>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <div className="container-fluid">
-            <a className="navbar-brand" href="/#">
-              My App
-            </a>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="/#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarSupportedContent"
-            >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/#">
-                    Home
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-      </React.Fragment>
-    );
+    return <NavBar />;
   }
 }
-
-export default NavBar;
