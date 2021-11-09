da0990a4659ed00f90da0444be45c5461c2a361b
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-09 14:25:41 -0800

    update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import MainContent from "./MainContent";
 export default class App extends Component {
   render() {
-    return <NavBar />;
+    return (
+      <React.Fragment>
+        <NavBar />
+        <MainContent />
+      </React.Fragment>
+    );
   }
 }
---
+++ src/MainContent.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from "react";
+export default class MainContent extends Component {
+  render() {
+    return <div> Hello from Main Content!</div>;
+  }
+}
