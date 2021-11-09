091ca08ce4c5ee66d980386e118a92aff7d4fd29
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-09 14:09:55 -0800

    fix /# bug

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ class NavBar extends Component {
       <React.Fragment>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/#">
               My App
             </a>
             <button
               className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
+              data-bs-target="/#navbarSupportedContent"
               aria-controls="navbarSupportedContent"
               aria-expanded="false"
               aria-label="Toggle navigation"
@@ -25,7 +25,7 @@ class NavBar extends Component {
             >
               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="#">
+                  <a className="nav-link active" aria-current="page" href="/#">
                     Home
                   </a>
                 </li>
