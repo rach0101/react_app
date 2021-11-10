baabd2acc762e7adaf43b38d206a4f900c9cfd3e
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-10 12:20:33 -0800

    Adding ecommerce state and components

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -6,7 +6,7 @@ class NavBar extends Component {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <a className="navbar-brand" href="/#">
-              My App
+              eCommerce
             </a>
             <button
               className="navbar-toggler"
