86c8a37f181b07472284ccfdf84b8b3a1881e7ee
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-09 16:29:43 -0800

    Update state of customersCount

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -10,8 +10,16 @@ export default class MainContent extends Component {
           <span className="badge bg-secondary m-2">
             {this.state.customersCount}
           </span>
+          <button className="btn btn-info" onClick={this.onRefreshClick}>
+            Refresh
+          </button>
         </h4>
       </div>
     );
   }
+
+  //execute when the user clicks on the Refresh button
+  onRefreshClick = () => {
+    this.setState({ customersCount: 7 });
+  };
 }
