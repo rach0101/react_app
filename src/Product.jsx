d9264e9d9fb9d7bd868e7e3d8927707ed515b37d
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-10 12:51:27 -0800

    adding product ids to shopping cart page

--- src/Product.jsx
+++ src/Product.jsx
@@ -2,6 +2,13 @@ import React, { Component } from "react";
 
 export default class Product extends Component {
   render() {
-    return <div>Product</div>;
+    console.log(this.props);
+    return (
+      <div className="card">
+        <div class="card-body">
+          <div class="text-muted">#{this.props.id} </div>
+        </div>
+      </div>
+    );
   }
 }
--- src/ShoppingCart.jsx
+++ src/ShoppingCart.jsx
@@ -4,11 +4,11 @@ export default class ShoppingCart extends Component {
   state = {
     products: [
       { id: 1, productname: "iphone", price: 8900, quantity: 0 },
-      { id: 1, productname: "iphone", price: 200, quantity: 0 },
-      { id: 1, productname: "keyboard", price: 90, quantity: 0 },
-      { id: 1, productname: "Macbook", price: 1200, quantity: 0 },
-      { id: 1, productname: "monitor", price: 890, quantity: 0 },
-      { id: 1, productname: "standing desk", price: 900, quantity: 0 },
+      { id: 2, productname: "iphone", price: 200, quantity: 0 },
+      { id: 3, productname: "keyboard", price: 90, quantity: 0 },
+      { id: 4, productname: "Macbook", price: 1200, quantity: 0 },
+      { id: 5, productname: "monitor", price: 890, quantity: 0 },
+      { id: 6, productname: "standing desk", price: 900, quantity: 0 },
     ],
   };
   render() {
@@ -17,7 +17,14 @@ export default class ShoppingCart extends Component {
         <h4>Shopping Cart</h4>
         <div>
           {this.state.products.map((prod) => {
-            return <Product />;
+            return (
+              <Product
+                key={prod.id}
+                id={prod.id}
+                productName={prod.productName}
+                price={prod.price}
+              />
+            );
           })}
         </div>
       </div>
