593331b466a1f5809b5ab7e66f65c9c07f1eae92
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-10 10:46:22 -0800

    More css testing

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -44,9 +44,9 @@ export default class MainContent extends Component {
   };
 
   customerNameStyle = (custName) => {
-    if (custName.startsWith("S")) return { backgroundColor: "green" };
-    else if (custName.startsWith("J")) return { backgroundColor: "red" };
-    else return {};
+    if (custName.startsWith("S")) return "green-highlight border-left";
+    else if (custName.startsWith("J")) return "red-highlight border-right";
+    else return "";
   };
   render() {
     return (
@@ -96,7 +96,7 @@ export default class MainContent extends Component {
           <td>
             <img src={cust.photo} alt="Customer" />
           </td>
-          <td style={this.customerNameStyle(cust.name)}>{cust.name}</td>
+          <td className={this.customerNameStyle(cust.name)}>{cust.name}</td>
           <td>{this.getPhoneToRender(cust.phone)}</td>
           <td>{cust.address.city}</td>
         </tr>
