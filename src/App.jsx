f12ace17359b2264876b3fc5be783bce5929497f
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-13 09:56:53 -0800

    adding Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
+import Dashboard from "./Dashboard";
 // import ShoppingCart from "./ShoppingCart";
-import Login from "./Login";
+// import Login from "./Login";
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
         <NavBar />
-        <Login />
+        <Dashboard />
       </React.Fragment>
     );
   }
