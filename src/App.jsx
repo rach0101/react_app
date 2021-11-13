fcde94c896e4cfe34e419a89531f1b072a06ac61
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-13 10:22:39 -0800

    Add Dashboard and no match page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
-// import ShoppingCart from "./ShoppingCart";
-// import Login from "./Login";
+import Login from "./Login";
+import ShoppingCart from "./ShoppingCart";
+import CustomersList from "./CustomersList";
+import { Route } from "react-router";
+import { Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import NoMatchPage from "./NoMatchPage";
+
 export default class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <NavBar />
-        <Dashboard />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <NavBar />
+          <Routes>
+            <Route path="/" exact component={Login} />
+            <Route path="/dashbord" exact component={Dashboard} />
+            <Route path="/customers" exact component={CustomersList} />
+            <Route path="/cart" exact component={ShoppingCart} />
+            <Route path="/*" component={NoMatchPage} />
+          </Routes>
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
