9fd0015595b33da62b1cbeace93a732a8959e926
Rachel Cleak <rachelmcleak@gmail.com>
2021-11-14 10:11:24 -0800

    bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import NavBar from "./NavBar";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import ShoppingCart from "./ShoppingCart";
 import CustomersList from "./CustomersList";
-// import { Route} from "react-router";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NoMatchPage from "./NoMatchPage";
 
@@ -15,11 +14,11 @@ export default class App extends Component {
         <React.Fragment>
           <NavBar />
           <Routes>
-            <Route path="/" exact component={Login} />
-            <Route path="/dashbord" exact component={Dashboard} />
-            <Route path="/customers" exact component={CustomersList} />
-            <Route path="/cart" exact component={ShoppingCart} />
-            <Route path="/*" component={NoMatchPage} />
+            <Route path="/" exact element={<Login />} />
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/customers" exact element={<CustomersList />} />
+            <Route path="/cart" exact element={<ShoppingCart />} />
+            <Route path="/*" element={<NoMatchPage />} />
           </Routes>
         </React.Fragment>
       </BrowserRouter>
--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 class NavBar extends Component {
   render() {
     return (
@@ -23,11 +24,29 @@ class NavBar extends Component {
               className="collapse navbar-collapse"
               id="navbarSupportedContent"
             >
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <ul className="navbar-nav mr-auto mb-2 mb-lg-0">
                 <li className="nav-item">
-                  <a className="nav-link active" aria-current="page" href="/#">
-                    Home
-                  </a>
+                  <Link to="/" className="nav-link">
+                    Login
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/dashboard" className="nav-link">
+                    Dashboard
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/customers" className="nav-link">
+                    Customers
+                  </Link>
+                </li>
+
+                <li className="nav-item">
+                  <Link to="/cart" className="nav-link">
+                    Shopping Cart
+                  </Link>
                 </li>
               </ul>
             </div>
